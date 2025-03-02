4f04cf81e8545787a20b5767eeed45dfea0b5336
nashim1magnetism <nashim@magnetism.com>
2025-03-02 09:50:18 +0600

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Deliveryorder;
+use App\EgmDeliveryorder;
+use App\EgmFeederinformation;
+use App\EgmHouseBl;
+use App\EgmMasterBl;
+use App\EgmMloblinformation;
+use App\EgmMloDeliveryorder;
+use App\EgmMloMoneyReceipt;
+use App\EgmMoneyreceipt;
 use App\Housebl;
 use App\Masterbl;
 use App\MLO\Feederinformation;
@@ -55,30 +63,30 @@ compact('totalMasterbl', 'masterblCurrentMonth','totalHousebl', 'houseblCurrentM
     }    
     public function egmDashboard()
     {
-        $totalMasterbl = Masterbl::all()->count();
-        $masterblCurrentMonth = Masterbl::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
+        $totalMasterbl = EgmMasterBl::all()->count();
+        $masterblCurrentMonth = EgmMasterBl::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
 
-        $totalHousebl = Housebl::all()->count();
-        $houseblCurrentMonth = Housebl::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
+        $totalHousebl = EgmHouseBl::all()->count();
+        $houseblCurrentMonth = EgmHouseBl::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
 
-        $totalMoneyReceipts = Moneyreceipt::all()->count();
-        $moneyReceiptCurrentMonth = Moneyreceipt::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
+        $totalMoneyReceipts = EgmMoneyreceipt::all()->count();
+        $moneyReceiptCurrentMonth = EgmMoneyreceipt::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
 
-        $totalDeliveryOrders = Deliveryorder::all()->count();
-        $deliveryOrderCurrentMonth = Deliveryorder::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
+        $totalDeliveryOrders = EgmDeliveryorder::all()->count();
+        $deliveryOrderCurrentMonth = EgmDeliveryorder::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
 
 
-        $totalFeeder = Feederinformation::all()->count();
-        $feederCurrentMonth = Feederinformation::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
+        $totalFeeder = EgmFeederinformation::all()->count();
+        $feederCurrentMonth = EgmFeederinformation::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
 
-        $totalBlInformation = Mloblinformation::all()->count();
-        $blInformationCurrentMonth = Mloblinformation::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
+        $totalBlInformation = EgmMloblinformation::all()->count();
+        $blInformationCurrentMonth = EgmMloblinformation::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
 
-        $totalMloMoneyReceipts = \App\MLO\MoneyReceipt::all()->count();
-        $mloMoneyReceiptsCurrentMonth = \App\MLO\MoneyReceipt::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
+        $totalMloMoneyReceipts = EgmMloMoneyReceipt::all()->count();
+        $mloMoneyReceiptsCurrentMonth = EgmMloMoneyReceipt::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
 
-        $totalMloDeliveryOrders = \App\MLO\Deliveryorder::all()->count();
-        $MloDeliveryOrderCurrentMonth = \App\MLO\Deliveryorder::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
+        $totalMloDeliveryOrders = EgmMloDeliveryorder::all()->count();
+        $MloDeliveryOrderCurrentMonth = EgmMloDeliveryorder::whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)->count();
 
         return view('egm',
 compact('totalMasterbl', 'masterblCurrentMonth','totalHousebl', 'houseblCurrentMonth','totalMoneyReceipts', 'moneyReceiptCurrentMonth','totalDeliveryOrders', 'deliveryOrderCurrentMonth',
